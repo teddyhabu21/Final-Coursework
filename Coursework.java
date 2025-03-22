997e8dcb25fc2a00a4741221267cc9ef68a47f21
HABU'S MacBook <macbookair@Tshewangs-MacBook-Air.local>
2025-03-22 00:04:43 +0000

    Defined a oublic class named MergeSort and a public static method that takes Arraylist of strings as inout. Additionally, a method to separate and add the elements in 2 halves recursively is added.

--- Coursework.java
+++ Coursework.java
@@ -2,4 +2,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// public class to perform the mergesort method
+public class Merge_Sort{
+    public static void mergeSort(ArrayList<String> list) {
+        if (list.size() <= 1) {
+            return; // when there is only 1 element or no element, it is already sorted.
+        }
+        
+        //splitting the list into two: 1) From the first element to the center point(cp) and 2) from cp to the last element.
+        int cp = list.size() / 2;
+        ArrayList<String> left = new ArrayList<>(list.subList(0, cp));
+        ArrayList<String> right = new ArrayList<>(list.subList(cp, list.size()));
+        
+        mergeSort(left);
+        mergeSort(right);
 
+        //Merge 
+       
+    }
+
+}
