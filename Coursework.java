ed8044eee33be4c833537226bd50cdb266c0e953
HABU'S MacBook <macbookair@Tshewangs-MacBook-Air.local>
2025-03-22 00:16:07 +0000

    Defined a method to merge back the sorted halves.

--- Coursework.java
+++ Coursework.java
@@ -11,8 +11,21 @@ public class Merge_Sort{
         
         //splitting the list into two: 1) From the first element to the center point(cp) and 2) from cp to the last element.
         int cp = list.size() / 2;
-        ArrayList<String> left = new ArrayList<>(list.subList(0, cp));
-        ArrayList<String> right = new ArrayList<>(list.subList(cp, list.size()));
+
+        // Create empty ArrayLists for left and right halves
+        ArrayList<String> left = new ArrayList<>();
+        ArrayList<String> right = new ArrayList<>();
+
+        // Copy elements to left half
+        for (int i = 0; i < cp; i++) {
+            left.add(list.get(i));
+        }
+
+        // Copy elements to right half
+        for (int i = cp; i < list.size(); i++) {
+            right.add(list.get(i));
+        }
+
         
         mergeSort(left);
         mergeSort(right);
@@ -20,5 +33,33 @@ public class Merge_Sort{
         //Merge 
        
     }
+    //Method to merge the two halves back together.
+     private static void merge(ArrayList<String> merged, ArrayList<String> left, ArrayList<String> right) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int mergeIndex = 0;
+        
+        while (leftIndex < left.size && rightIndex < right.size){
+            if (left[leftIndex] <= right[rightIndex]){
+                merged[mergeIndex] = left[leftIndex];
+                leftIndex++;
+            }
+            else{
+                merged[mergeIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            mergeIndex++;
+        }
+        while (leftIndex < left.size){
+            inputArray[mergeIndex] = left[leftIndex];
+            leftIndex++;
+            mergeIndex++;
+        }
+        while (rightIndex < right.size){
+            inputArray[mergeIndex] = right[rightIndex];
+            rightIndex++;
+            mergeIndex++;
+        }
+    }    
 
 }
