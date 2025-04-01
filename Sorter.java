a64ba869f9cbbba408ef29bfb6e7b9396cfa2a2d
HABU'S MacBook <macbookair@Tshewangs-MacBook-Air.local>
2025-04-01 18:36:22 +0100

    I renamed the file 'Coursework.java' to 'Sorter.java' using the mv command.

--- Sorter.java
+++ Sorter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class mergeSort{
     public static ArrayList<String> splitSort(ArrayList<String> list) {
         if (list.size() <= 1) {
-            return list; // when there is only 1 element or no element, it is already sorted.
+            return list; // when there is only 1 element or no element, it is already sorted, so it should return itself.
         }
         
         //splitting the list into two: 1) From the first element to the center point(cp) and 2) from cp to the last element.
@@ -28,6 +28,7 @@ public class mergeSort{
         int leftIndex = 0;
         int rightIndex = 0;
         
+        //creating an empty new list to store the sorted list
         ArrayList<String> merged = new ArrayList<>();
         
         while (leftIndex < left.size() && rightIndex < right.size()){
@@ -53,14 +54,16 @@ public class mergeSort{
         }
         return merged;
     }
+
+
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<>(List.of("January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"));
-        System.out.println("Before sorting: " + words);
+        System.out.println("Before sorting: " + words); //printing out the raw list.
         
         words = splitSort(words);
         
-        System.out.println("After sorting: " + words);
+        System.out.println("After sorting: " + words);//printing the sorted list
     }    
 
 }
