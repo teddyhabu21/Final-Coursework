10fdf7335b13bc918f75371e20e54ebfb0e759ac
HABU'S MacBook <macbookair@Tshewangs-MacBook-Air.local>
2025-04-03 12:14:15 +0100

    Added a test method for the months for the second part of the question.

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,10 @@
 public class MyClass {
     public static void main(String args[]) {
         sorterTest.test1();
-        System.out.println("done");
+        System.out.println("Test 1 is successful");
+        
+        sorterTest.test2();
+        System.out.println("Test for the months is successful.");
     }
+    
 }
--- sorterTest.java
+++ sorterTest.java
@@ -1,20 +1,44 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class sorterTest {
-
     @Test
     public static void test1() {
         ArrayList<String> words = new ArrayList<>(List.of("b", "c", "a"));
         String[] expected = new String[]{"a", "b", "c"};
-        String[] actual = Sorter.splitSort(words).toArray(new String[0]); // Convert to array ✅
+        String[] actual = Sorter.splitSort(words).toArray(new String[0]); 
+        //the splitSort method from the Sorter class is being called with the words Arraylist. 
+        //Then the result of the method is converted back to array.
         
-        System.out.println("Expected: " + List.of(expected));
-        System.out.println("Actual: " + List.of(actual));
+        System.out.println("Expected list: " + List.of(expected));
+        System.out.println("Actual Outcome: " + List.of(actual));
         
+        //the test asserts that the expected array id equal to the actual outcome 
+        //of the method using jUnit's assertArrayEquals method.
         assertArrayEquals(expected, actual);
         
+    }
+    
+    // this part is for the second part of the question checking if the method 
+    //sorts out the months in alphabetical order.
+    @Test
+    public static void test2() {
+        ArrayList<String> months = new ArrayList<>(List.of("January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"));
+        String[] expected = new String[]{"April", "August", "December", "February", "January", "July",
+            "June", "March", "May", "November", "October", "September"};
+        String[] actual = Sorter.splitSort(months).toArray(new String[0]); 
+        //the splitSort method from the Sorter class is being called with the months Arraylist. 
+        //Then the result of the method is converted back to array.
+        
+        System.out.println("Expected list: " + List.of(expected));
+        System.out.println("Actual Outcome: " + List.of(actual));
+        
+        //the test asserts that the expected array id equal to the actual outcome 
+        //of the method using jUnit's assertArrayEquals method.
+        assertArrayEquals(expected, actual);
     }
 }
